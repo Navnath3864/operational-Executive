5ef0aa5fb638fec416d51fea1035d831ff84d99a
Navnath3864 <nagutte0@gmail.com>
2025-01-22 15:41:49 +0530

    add oe in requestmapping

--- OperationExecutive/src/main/java/com/app/controller/OperationalExecutiveController.java
+++ OperationExecutive/src/main/java/com/app/controller/OperationalExecutiveController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import com.app.model.CibilScoreData;
 import com.app.model.EnquiryDetails;
 
 @RestController
+@RequestMapping("/oe")
 public class OperationalExecutiveController {
 	@Autowired
 	RestTemplate rs;
