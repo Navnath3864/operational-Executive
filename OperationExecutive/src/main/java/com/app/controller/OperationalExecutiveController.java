a139452951368b0c5d4df6bb263d21bbcf653f6b
aman <77562702+amankhadse461@users.noreply.github.com>
2025-01-22 17:45:59 +0530

    perform email sending task and create sentmailtocustomer methods

--- OperationExecutive/src/main/java/com/app/controller/OperationalExecutiveController.java
+++ OperationExecutive/src/main/java/com/app/controller/OperationalExecutiveController.java
@@ -1,10 +1,14 @@
 package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,15 +19,39 @@ import com.app.model.EnquiryDetails;
 @RestController
 @RequestMapping("/oe")
 public class OperationalExecutiveController {
+
+	@Autowired
+	JavaMailSender sender;
+
+	@Value("${spring.mail.username}")
+	private static String SEND_FROM;
 	@Autowired
 	RestTemplate rs;
+
 	@GetMapping("/getenquirydata/{customerID}")
 	public ResponseEntity<EnquiryDetails> getenquirydata(@PathVariable int customerID) {
-		String urlToGetEnquiryData="http://localhost:8080/app/api/enquiry/"+customerID;
-		EnquiryDetails enq=rs.getForObject(urlToGetEnquiryData, EnquiryDetails.class) ;
-		String urlToGetCibilData="http://localhost:9090/api/cibil/get";
-		CibilScoreData cibil=rs.getForObject(urlToGetCibilData, CibilScoreData.class);
+		String urlToGetEnquiryData = "http://localhost:8080/app/api/enquiry/" + customerID;
+		EnquiryDetails enq = rs.getForObject(urlToGetEnquiryData, EnquiryDetails.class);
+		String urlToGetCibilData = "http://localhost:9090/api/cibil/get";
+		CibilScoreData cibil = rs.getForObject(urlToGetCibilData, CibilScoreData.class);
 		enq.setCibilScoreData(cibil);
-		return new ResponseEntity<EnquiryDetails>(enq,HttpStatus.OK);
- 	}
+		sentMailToCusotmer(enq);
+		return new ResponseEntity<EnquiryDetails>(enq, HttpStatus.OK);
+	}
+
+	public void sentMailToCusotmer(EnquiryDetails enquiryDetails) {
+		String cibilRemark = enquiryDetails.getCibilScoreData().getCibilRemark();
+		String email = enquiryDetails.getEmail();
+		System.out.println(email);
+		System.out.println(cibilRemark);
+		if (cibilRemark == "Applicable") {
+			System.out.println(cibilRemark);
+			SimpleMailMessage simple = new SimpleMailMessage();
+			simple.setTo(email);
+			simple.setFrom(SEND_FROM);
+			simple.setSubject("Your Loan Request is Approved!");
+			simple.setText("Thank you!");
+			sender.send(simple);
+		}
+	}
 }
