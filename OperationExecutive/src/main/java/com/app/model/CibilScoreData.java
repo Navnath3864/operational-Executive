24d1de0accfefac6086ed6a6dcf4e4a8d8ea9170
aman <77562702+amankhadse461@users.noreply.github.com>
2025-01-24 15:19:36 +0530

    ..

--- OperationExecutive/src/main/java/com/app/model/CibilScoreData.java
+++ OperationExecutive/src/main/java/com/app/model/CibilScoreData.java
@@ -1,16 +1,11 @@
 package com.app.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+
 import lombok.Data;
 
-@Entity
 @Data
 public class CibilScoreData {
-	@Id
+
 	private int cibilId;
 	private int cibilScore;
 	private String cibilScoreDateTime;
--- OperationExecutive/src/main/java/com/app/model/EnquiryDetails.java
+++ OperationExecutive/src/main/java/com/app/model/EnquiryDetails.java
@@ -1,19 +1,10 @@
 package com.app.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 
-@Entity
+
 @Data
 public class EnquiryDetails {
-	@Id
 	private int customerID;
 	private String firstName;
 	private String lastName;
@@ -23,8 +14,7 @@ public class EnquiryDetails {
 	private String enquiryStatus ="pending";
 	private String pancardNo;
 	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "cibil_id")
+
 	private CibilScoreData cibilScoreData;
 
 //	"firstName":"Harshada",
